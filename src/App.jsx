fddbbcc57923a88e9559450ca2ae6cbdb5672e60
Saurabh kumar <saurabh4442kumar@gmail.com>
2025-03-27 13:52:19 +0530

    fix5

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import AllToDoList from "./Components/AllToDoList";
 import ViewToDo from "./Components/ViewToDo";
 import Login from "./Components/Login";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <Navbar/> 
-        <Home/>
-        <AllToDoList/>
-      </div>
-    ),
-  },
-  {
-    path: "/Notes",
-    element: (
-      <div>
-        <Navbar /> <AllToDoList/>
-      </div>
-    ),
-  },
-  {
-    path: "/Notes/:id",
-    element: (
-      <div>
-        <Navbar /> <ViewToDo/>
-      </div>
-    ),
-  },
-]);
-
-function App() {
+const App = () => {
   const [user, setUser] = useState(false);
 
   const handleLogin = (email, password) => {
@@ -45,11 +16,36 @@ function App() {
     }
   };
 
-  return (
-    <>
-      {!user ? <Login handlelogin={handleLogin} /> : <RouterProvider router={router} />}
-    </>
-  );
-}
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: (
+        <div>
+          <Navbar />
+          <Home />
+          <AllToDoList />
+        </div>
+      ),
+    },
+    {
+      path: "/Notes",
+      element: (
+        <div>
+          <Navbar /> <AllToDoList />
+        </div>
+      ),
+    },
+    {
+      path: "/Notes/:id",
+      element: (
+        <div>
+          <Navbar /> <ViewToDo />
+        </div>
+      ),
+    },
+  ]);
+
+  return <>{!user ? <Login handlelogin={handleLogin} /> : routes}</>;
+};
 
 export default App;
